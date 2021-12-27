ef2e26acd8d27e6b198163168a6a6c829f984460
Andrew Barba <barba@hey.com>
2021-12-27 14:43:26 -0500

    Initial Commit

---
+++ Shared/Fast_FoodzApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fast_FoodzApp.swift
+//  Shared
+//
+//  Created by Andrew Barba on 12/27/21.
+//
+
+import SwiftUI
+
+@main
+struct Fast_FoodzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
